27c6ca9ddd1cbd2fc40931ac7cf814cdb4963e49
Manikanta <manikantagadde123@gmail.com>
2017-07-13 12:26:40 +0530

    Modifies some files

--- Java_Assingment-1/src/FilePath.java
+++ Java_Assingment-1/src/FilePath.java
@@ -1,8 +1,15 @@
-/**
- * Created by zemosozemoso on 7/7/17.
- */
 import java.io.*;
 import java.util.*;
+
+/**
+ * Question: Create an java program to search through the home directory and look for files
+ * that match a regular expression. The program should be able to take inputs repeatedly and
+ * should print out full absolute path of the matching files found.
+ *
+ * @author Manikanta.
+ * Created by zemosozemoso on 7/7/17.
+ */
+
 public class FilePath
 {
     public static void main(String[] args) {
@@ -14,22 +21,25 @@ public class FilePath
             String directory = "/home/";  // Directory name, where we want to search the files.
             fp.findFile(filename, new File(directory));
             System.out.println();
-            System.out.println("Enter the file name to search the path or exit by typing 'exit' : ");  
+            System.out.println("Enter the file name to search the path or exit by typing 'exit' : ");
         }
-            scan.close();
+        scan.close();
     }
 
-    public void findFile(String filename,File dir) {
+    /**
+     * @param filename Indicates the filename to be given. Filename with type should be given. E.g: Text.txt
+     * @param dir Indicates the directory to search for a given file name.
+     */
+    void findFile(String filename, File dir) {
         File[] dirlist = dir.listFiles();
         if(dirlist!=null)
             for (File filordir : dirlist) {
                 if (filordir.isDirectory()) {   /* checks weather file or directory */
-                    findFile(filename,filordir);
+                    findFile(filename,filordir); /* Recursively finds the file. */
                 }
                 else if (filename.equalsIgnoreCase(filordir.getName())) {
                     System.out.println(filordir.getAbsolutePath());
                     /* Prints the absolute pathname string of this abstract pathname. */
-
                 }
             }
     }
---
+++ Java_Assingment-1/src/FilePathTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Question: Test cases for Java Assingment 1. Tested using some of files in my Home directory.
+ * @author Manikanta
+ * Created by zemosozemoso on 13/7/17.
+ */
+
+public class FilePathTest {
+    private FilePath filePath = new FilePath();
+    @Test
+    public void Test1() {
+        filePath.findFile("Track.txt", new File("/Home/"));
+    }
+    @Test
+    public void Test2() {
+        filePath.findFile("codeconventions-150003.pdf", new File("/Home/"));
+    }
+    @Test
+    public void Test3() {
+        filePath.findFile("Photo1837.jpg", new File("/Home/"));
+    }
+
+}
